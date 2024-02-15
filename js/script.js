6e8144e179b83a9213d93c41354207799db11901
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-15 11:32:19 +0500

    index html

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+
+// let yosh = '23'; //  string
+// console.log('yosh');//  string
+// console.log(Number(yosh));
+
+// console.log(yosh+1);
+// console.log(Number(yosh)+1);
+
+
+// let ism = 'samandar';
+// console.log(Number(ism));
+
+
+// let yil = '2021';
+// console.log(yil);
+// console.log(String(yil));
+
+
+
+
+// console.log('Men ' + 2004 + ' chi yilda tavalluda topganman' );
+
+// console.log('30'-'10'-5);
+
+// console.log(Boolean(false));
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(0n));
+
+// console.log(Boolean('samnadar'));
+// console.log(Boolean(23));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+// console.log(Boolean('0'));
+// console.log(Boolean('false'));
+
+
+// let ism = '';
+
+// if (ism) {
+//     console.log('sizning ismingi: ' + ism);
+// } else {
+//     console.log('iltimos ism kiriting');
+// }
+
+
+
+
